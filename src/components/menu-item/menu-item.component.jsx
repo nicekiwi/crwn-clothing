fd4fdd540ce6f47f84f14337b64e1da3476426a7
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-01-23 17:40:29 +1300

    split into components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.component.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }}></div>
+        <div className='content'>
+            <h1 className='title'>{ title.toUpperCase() }</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
