8d48dab05e6f3c0cd44dbdda442e5aff229e748b
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-01-24 14:36:24 +1300

    Add collection item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/colection-item.component'
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items }) => (
@@ -9,8 +10,8 @@ const CollectionPreview = ({title, items }) => (
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                    .map(({ id, ...otherItemProps }) => (
+                        <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
