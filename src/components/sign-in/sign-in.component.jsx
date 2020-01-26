c7c09def5fd3d597d7116461f2f35840fbeb53de
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-01-27 11:25:28 +1300

    CustomButton component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -46,7 +47,7 @@ class SignIn extends Component {
                         value={this.state.password} 
                         required
                         handleChange={this.handleChange} />
-                    <input type='submit' />
+                    <CustomButton type='submit'> Sign In </CustomButton>
                 </form>
             </div>
         );
