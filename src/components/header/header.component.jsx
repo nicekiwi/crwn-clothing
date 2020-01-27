6c22a74453aea217bfcfeb1d39122e94527bc81c
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-01-27 20:28:35 +1300

    add redux and reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -30,6 +31,10 @@ const Header = ({ currentUser }) => (
             }
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
