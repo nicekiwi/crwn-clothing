e2447ff2e022b5604b55c70939a7d434881dc308
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-01-30 12:02:09 +1300

    setup dynamic routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 import './shop.styles.scss';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
