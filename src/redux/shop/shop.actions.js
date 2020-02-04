a77fd5278085d93d72d1e84940ea0b6a1902dac7
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-02-04 18:19:00 +1300

    start thunk intergration

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,20 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+            this.setState({ isLoading: false });
+        })
+    }
+} 
